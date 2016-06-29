e9daf1bcb0878593771d10853ca185d36367ca3c
Peter Zastoupil <peterzastoupil@gmail.com>
2016-06-29 17:30:29 -0400

    added remaining api model classes

---
+++ java-server/src/main/java/com/cooksys/ftd/celebrity/news/model/api/GetUserByUsername.java
@@ -0,0 +1,13 @@
+package com.cooksys.ftd.celebrity.news.model.api;
+
+public class GetUserByUsername {
+	private String username;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+}
---
+++ java-server/src/main/java/com/cooksys/ftd/celebrity/news/model/api/IndexActors.java
@@ -0,0 +1,5 @@
+package com.cooksys.ftd.celebrity.news.model.api;
+
+public class IndexActors {
+	// indicates client wanted all actors; no data required
+}
