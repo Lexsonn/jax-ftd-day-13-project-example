f0f78e91173ae0a9823ae500181e2a8af219e887
Peter Zastoupil <peterzastoupil@gmail.com>
2016-06-29 17:00:28 -0400

    implemented server

--- java-server/src/main/java/com/cooksys/ftd/celebrity/news/server/Server.java
+++ java-server/src/main/java/com/cooksys/ftd/celebrity/news/server/Server.java
@@ -1,14 +1,23 @@
 package com.cooksys.ftd.celebrity.news.server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
-import java.sql.Connection;
+import java.net.Socket;
 import java.util.concurrent.ExecutorService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.cooksys.ftd.celebrity.news.dao.ActorDao;
 import com.cooksys.ftd.celebrity.news.dao.UserActorDao;
 import com.cooksys.ftd.celebrity.news.dao.UserDao;
 
 public class Server implements Runnable {
+	
+	private Logger log = LoggerFactory.getLogger(Server.class);
 
 	private ExecutorService executor;
 	private ServerSocket serverSocket;
@@ -19,7 +28,30 @@ public class Server implements Runnable {
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		try {
+			while (true) {
+				Socket socket = this.serverSocket.accept();
+				ClientHandler handler = this.createClientHandler(socket);
+				this.executor.execute(handler);
+			}
+		} catch (IOException e) {
+			this.log.error("The server encountered a fatal error while listening for more connections. Shutting down after error log.", e);
+		}
+	}
+
+	public ClientHandler createClientHandler(Socket socket) throws IOException {
+		ClientHandler handler = new ClientHandler();
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		handler.setReader(reader);
+		PrintWriter writer = new PrintWriter(socket.getOutputStream());
+		handler.setWriter(writer);
+
+		handler.setActorDao(actorDao);
+		handler.setUserActorDao(userActorDao);
+		handler.setUserDao(userDao);
+
+		return handler;
 	}
 
 	public ExecutorService getExecutor() {
